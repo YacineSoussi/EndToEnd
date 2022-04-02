62aade6e8696139fbcb7d3002d9e944d99d6f943
YacineSoussi <Yacine1.soussi@gmail.com>
2022-04-02 18:23:05 +0200

    Install drivers & ContactTests

--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -9,13 +9,15 @@ class ContactTest extends PantherTestCase
     public function testInvalidEmail(): void
     {
         $client = static::createPantherClient();
+
         $crawler = $client->request('GET', '/contact');
         $form = $crawler->selectButton('Submit')->form([
             'contact[email]' => 'Yacine',
             'contact[subject]' => 'Mon sujet',
             'contact[email]' => 'Voici ma demande très spéciale.'
         ]);
+        $client->submit($form);
 
-        $this->assertSelectorTextContains('invalid-feedback', 'This value is not a valid email address.');
+        $this->assertSelectorTextContains('.invalid-feedback', 'This value is not a valid email address.');
     }
 }
